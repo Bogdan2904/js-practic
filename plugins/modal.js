6c3d2b40dd630e988bdd9ba8633d56f16615354f
bodan <bbolyubash@icloud.com>
2020-07-18 17:30:53 +0300

    Initial commit

---
+++ plugins/modal.js
@@ -0,0 +1,14 @@
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('bmodal')
+}
+
+
+$.modal = function (options) {
+
+    return{
+        open(){},
+        close(){},
+        destroy(){}
+    }
+}
